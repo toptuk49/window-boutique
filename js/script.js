227d619935d45acf17d857601c05906501004c47
toptuk49 <69755076+toptuk49@users.noreply.github.com>
2022-06-27 14:44:35 +0300

    Первая версия

---
+++ js/script.js
@@ -0,0 +1,60 @@
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    }
+    
+    testWebP(function (support) {
+    
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
+
+function fontsStyle(params) {
+
+    let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let c_fontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (c_fontname != fontname) {
+                        fs.appendFile(source_folder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    c_fontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {}
+
+// Слайдер
+let slider = document.querySelector('.slider-line');
+let offset = 0;
+
+document.querySelector('.slider-next-button').addEventListener('click', function() {
+  offset += 386;
+  if ( offset > 1544 ) {
+    offset = 0;
+  }
+  slider.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev-button').addEventListener('click', function() {
+  offset -= 386;
+  if ( offset < 0 ) {
+    offset = 1544;
+  }
+  slider.style.left = -offset + 'px';
+});
